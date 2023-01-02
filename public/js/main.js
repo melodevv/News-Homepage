5a5f2fada3a0ab173b8d97cca62de5f2a9f68205
TlhokomeloDireko <tdireko16@gmail.com>
2023-01-02 13:13:44 +0200

    First Commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,15 @@
 // Variable declarations
-const hamburgerOpen = document.getElementById("hamburger");
+const hamburgerOpen = document.getElementById("menu-open");
+const hamburgerClose = document.getElementById("menu-close");
 const menu = document.getElementById("menu");
+const nav = document.querySelectorAll(".nav-links");
 
 hamburgerOpen.onclick = function () {
-	this.classList.toggle("open-hamburger");
-	menu.classList.toggle("open-menu");
+	this.classList.add("open-hamburger");
+	menu.classList.add("open-menu");
+};
+
+hamburgerClose.onclick = function () {
+	hamburgerOpen.classList.remove("open-hamburger");
+	menu.classList.remove("open-menu");
 };
